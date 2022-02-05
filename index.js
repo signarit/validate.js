1af05982a963a97eadd051a334d3153fb3321f9f
mikkjal <mikkjaln@gmail.com>
2022-02-05 23:08:37 +0000

    build cjs

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
 'use strict';
 
 const rules = {
-    required({ value }) {
-        return !!value;
+    after({ value, args }) {
+        if (!new Date(value).getTime() || !new Date(args).getTime()) {
+            return false;
+        }
+
+        return new Date(value).getTime() > new Date(args).getTime();
     },
 
-    email({ value }) {
-        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-            value
-        );
+    ascii({ value }) {
+        return !/[^\x00-\x7F]+\ *(?:[^\x00-\x7F]| )*/g.test(value);
     },
 
-    regex({ value, args }) {
-        args = args.replace(/\//g, '');
+    before({ value, args }) {
+        if (!new Date(value).getTime() || !new Date(args).getTime()) {
+            return false;
+        }
 
-        return RegExp(args).test(value);
+        return new Date(value).getTime() < new Date(args).getTime();
     },
 
-    same({ value, args, form }) {
-        return value == form[args];
+    between({ value, args }) {
+        const [min, max] = args.split(',');
+
+        if (isNaN(value)) {
+            return false;
+        }
+
+        value = Number(value);
+
+        if (value < min || value > max) {
+            return false;
+        }
+
+        return true;
+    },
+
+    confirmed({ value, form, key }) {
+        return value == form[`${key}_confirmation`];
+    },
+
+    date({ value }) {
+        return !!new Date(value).getTime();
     },
 
     different(options) {
         return !this.same(options);
     },
 
-    confirmed({ value, form, key }) {
-        return value == form[`${key}_confirmation`];
+    email({ value }) {
+        return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g.test(
+            value
+        );
+    },
+
+    ends_with({ value, args }) {
+        return RegExp(`${args}$`).test(value);
+    },
+
+    hex({ value }) {
+        return /^#([\da-fA-F]{3}){1,2}$/g.test(value);
     },
 
     in({ value, args }) {
@@ -35,25 +69,34 @@ const rules = {
         return args.includes(value);
     },
 
+    ip({ value }) {
+        return /\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b/gi.test(value);
+    },
+
+    max({ value, args }) {
+        return value.length <= Number(args);
+    },
+
+    min({ value, args }) {
+        return value.length >= Number(args);
+    },
+
     not_in(options) {
         return !this.in(options);
     },
 
-    starts_with({ value, args }) {
-        return RegExp(`^${args}`).test(value);
+    numeric({ value }) {
+        return !isNaN(value);
     },
 
-    ends_with({ value, args }) {
-        return RegExp(`${args}$`).test(value);
+    regex({ value, args }) {
+        args = args.replace(/\//g, '');
+
+        return RegExp(args).test(value);
     },
 
-    json({ value }) {
-        try {
-            JSON.parse(value);
-            return true;
-        } catch (_) {
-            return false;
-        }
+    required({ value }) {
+        return !!value;
     },
 
     required_with({ value, args, form }) {
@@ -96,56 +139,16 @@ const rules = {
         return !this.required_with_all(options);
     },
 
-    // Number Rules
-
-    numeric({ value }) {
-        return !isNaN(value);
+    same({ value, args, form }) {
+        return value == form[args];
     },
 
-    min({ value, args }) {
-        return value.length >= Number(args);
+    starts_with({ value, args }) {
+        return RegExp(`^${args}`).test(value);
     },
 
-    max({ value, args }) {
-        return value.length <= Number(args);
-    },
-
-    between({ value, args }) {
-        const [min, max] = args.split(',');
-
-        if (isNaN(value)) {
-            return false;
-        }
-
-        value = Number(value);
-
-        if (value < min || value > max) {
-            return false;
-        }
-
-        return true;
-    },
-
-    // Date Rules
-
-    date({ value }) {
-        return !!new Date(value).getTime();
-    },
-
-    after({ value, args }) {
-        if (!new Date(value).getTime() || !new Date(args).getTime()) {
-            return false;
-        }
-
-        return new Date(value).getTime() > new Date(args).getTime();
-    },
-
-    before({ value, args }) {
-        if (!new Date(value).getTime() || !new Date(args).getTime()) {
-            return false;
-        }
-
-        return new Date(value).getTime() < new Date(args).getTime();
+    url({ value }) {
+        return /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.test(value);
     },
 };
 
